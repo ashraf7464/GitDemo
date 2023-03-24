e662679cbf13b00d25be0856bfe2001384444968
Md Ashraf Siddique <ashraf@Ashrafs-MBP.lan>
2023-03-23 21:26:07 -0400

    first commit

---
+++ NestedLoop.java
@@ -0,0 +1,32 @@
+package coreJava;
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int k=1;
+		for(int i=0; i<4; i++)
+		{
+			
+			//System.out.println("Outer Loop");
+			
+			
+			for(int j=1; j<=4-i; j++)
+			{
+				
+				System.out.print(k);
+				System.out.print("\t");
+				k++;
+				
+			}
+			
+			System.out.println("");
+			
+			
+		}
+			
+		
+	}
+
+}
---
+++ NestedLoop3.java
@@ -0,0 +1,36 @@
+package coreJava;
+
+public class NestedLoop3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		
+		for(int i=1;i<5; i++)
+		{
+			
+			
+			for(int j=1;j<=i; j++)
+				
+			{
+				System.out.print(j);
+				System.out.print("\t");
+				
+			}
+			
+			
+			
+			System.out.println("");
+			
+			
+		}
+		
+		
+		
+		
+			
+		
+	}
+
+}
